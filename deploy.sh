4429c667c99e1e76650e3f8dde9185bd70d9bd3f
mbellman <mbellman@nwe.com>
2018-03-11 01:11:49 -0600

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ read -p "Version bump (major | minor | patch): " bump;
 case $bump in
   "major" | "minor" | "patch")
     npm version $bump -m "Deploying %s";
+    git push
     npm publish
     ;;
   *)
