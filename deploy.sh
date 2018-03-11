6599388f949b8663909996d54e58b38004862dc3
mbellman <mbellman@nwe.com>
2018-03-11 00:03:49 -0600

    Updating the deploy script yet again

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
 yarn build
-git add .
 
 read -p "Commit message: " -i "no-message" message;
 read -p "Version bump: " bump;
 
 case $bump in
   "major" | "minor" | "patch")
+    git add .
     npm version $bump -m "Rebuilding and deploying %s ($message)";
+    npm publish
+    git push
     ;;
   *)
     echo "Invalid version bump!"
-    exit 0;
     ;;
 esac
-
-git push
