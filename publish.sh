05d7d5577355395d1be02099b04e42f82630adcb
mbellman <mbellman@nwe.com>
2018-03-10 23:43:30 -0600

    Adding a little deploy script and a package.json license

---
+++ publish.sh
@@ -0,0 +1,12 @@
+read -p "Commit message: " -i "no-message" message;
+read -p "Version bump: " bump;
+
+case $bump in
+  "major" | "minor" | "patch")
+    npm version $bump -m "Rebuilding and deploying %s ($message)";
+    ;;
+  *)
+    echo "Invalid version bump!"
+    exit 0;
+    ;;
+esac
